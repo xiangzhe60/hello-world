34c2b4b739966fedaee0fd892cc05c335f0a41ce
xiangzhe60 <69695470+xiangzhe60@users.noreply.github.com>
2020-08-14 13:00:42 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	    System.out.println("My name is Xiang Zhe but I perfered to be called Leo");
+	    System.out.println("My hobbie is playing tennis");
+	    System.out.println("And my favorit videogame is Rainbow Six Siege");
+	    System.out.println("I want to go to college at UCLA");
+	    System.out.println("I am considering to major somewhere in marketing");
+	
+	    }
+}
